b807e2d5b5384ac00353dde6eededd5ff9521bd6
Damian Kreick <damian.kreick@gmail.com>
2018-03-01 14:28:51 -0300

    Adds interface

---
+++ Simple Simon/Assets/DoLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoLogic : MonoBehaviour 
+{
+	public GameObject[] buttons = new GameObject[4];
+	public List<int> sequence = new List<int>();
+
+	void Start()
+	{
+		int numberToAdd = Random.Range (1, 4);
+		sequence.Add (numberToAdd);
+	}
+
+	public void HighlightButton(int index)
+	{
+		Debug.Log ("Hola");
+		if (index < sequence.Count) {
+			float alpha = buttons [sequence[index]].GetComponent<CanvasGroup> ().alpha;
+			
+			while (alpha < 1) 
+			{
+				alpha += 0.1f * Time.deltaTime;
+			}
+			alpha = 0.33f;
+		}
+
+		int numberToAdd = Random.Range (1, 4);
+		sequence.Add (numberToAdd);
+
+		HighlightButton(index++);
+	}
+}
